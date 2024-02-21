dccf19ca1a17eae4d51374c4fef55c3f097211c8
Aniket <aniketkumarraj0000@gmail.com>
2024-02-21 18:59:55 +0530

    step-17

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:snap_notes/constants/routes.dart';
 import 'package:snap_notes/services/auth/auth_service.dart';
-import 'package:snap_notes/views/notes/notes_view.dart';
-import 'package:snap_notes/views/verify_email_views.dart';
 import 'package:snap_notes/views/login_view.dart';
+import 'package:snap_notes/views/notes/create_update_note_view.dart';
+import 'package:snap_notes/views/notes/notes_view.dart';
 import 'package:snap_notes/views/register_view.dart';
-import 'package:snap_notes/views/notes/new_note_view.dart';
+import 'package:snap_notes/views/verify_email_views.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,40 +13,40 @@ void main() {
     MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.cyan
+        primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
       routes: {
-        loginRoute:(context) => const LoginView(),
-        registerRoute:(context) => const RegisterView(),
-        notesRoute:(context) => const NotesView(),
-        verifyEmailRoute:(context) => const VerifyEmailView(),
-        newNoteRoute:(context) => const NewNoteView(),
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
+        verifyEmailRoute: (context) => const VerifyEmailView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: AuthService.firebase().initialize(),
       builder: (context, snapshot) {
-        switch (snapshot.connectionState){
+        switch (snapshot.connectionState) {
           case ConnectionState.done:
-          final user = AuthService.firebase().currentUser;
-          if(user != null) {
-            if(user.isEmailVerified) {
-              return const NotesView();
-            }else{
-              return const VerifyEmailView();
+            final user = AuthService.firebase().currentUser;
+            if (user != null) {
+              if (user.isEmailVerified) {
+                return const NotesView();
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
             }
-          }else {
-            return const LoginView();
-          }
           default:
             return const CircularProgressIndicator();
         }
@@ -54,4 +54,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
